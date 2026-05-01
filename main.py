b64e66138a20c2de1bf06af7e8bd02d3afbb8337
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-05-01 16:50:16 +0500

    darsdagi kod

---
+++ main.py
@@ -0,0 +1,100 @@
+import os
+os.system("cls")
+
+# def salom_ber():
+#     print("Salom hammaga")
+
+# salom_ber()
+# salom_ber()
+# salom_ber()
+
+def func1():
+    pass
+
+# def func2(a, b):
+#     print(a + b)
+# func2("Salom ", "Dunyo")
+# func2("Salom ", 11)
+
+# def func3(a, b):
+#     c = a+b
+#     return c
+
+# print(func3(2,5))
+
+# def func4(a, b): 
+#     c = a + b
+#     d = a * b
+#     return c, d
+# a, b = func4(4,6)
+# print(a, b)
+# print(func4(4,6))
+
+# def func5(a,b,c=0):
+#     print(a+b+c)
+# func5(3,4)
+
+# def func6(a: int,b: int) ->int:
+#     print(a+b)
+#     return a*b 
+# # func6("salom","Dunyo")
+# n = func6(4,7)
+# print(n)
+# func6(5,8)
+
+# def func7(*a):
+#     print(sum(a))
+   
+# func7(2,3,4,5,6,7,8)
+
+
+# def func8(**n):
+#     print(n)
+#     print(n['ism']
+
+# func8(yosh = 15, ism = 'Doston',guruh = 'N205')
+
+
+# def func9(a,b):
+#     return a+b 
+
+# func10 = lambda a,b: a+b 
+
+# print(func9(4,5))
+# print(func10(4,5))
+
+
+
+#                                           Filter 
+
+
+# lst1 = [10,90,60,45,65,34,22,89]
+
+# lst2 = []
+# for i in lst1:
+#     if i%2==0:
+#         lst2.append(i)
+# print(lst2)
+
+
+# def is_even(n):
+#     return n%2==0
+# lst2 = list(filter(is_even,lst1))
+# print(lst2)
+
+
+# lst2 = list(filter(lambda n: n%2==0,lst1))
+# print(lst2)
+
+#                                       Map
+
+# ages = [19,20,34,21,38]
+
+# def func1(n):
+#     return n+1
+# lst2 = list(map(func1,ages))
+# print(lst2)
+
+# lst2 = list(map(lambda n: n+1,ages))
+# print(lst2)
+
