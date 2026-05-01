82bffcc917cce1af7e6a1f40e9ee490a529998af
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-05-01 16:51:31 +0500

    uy ishi3

---
+++ mashqlar3.py
@@ -0,0 +1,24 @@
+import os
+os.system("cls")
+
+#                     FILTER va MAP
+
+
+
+# 1.m
+# lst1 = ["Alisher","Kamoliddin", "abdulla", "Feruza", "Jamshid", "Dilnoza", "farhod", "Murod"]
+
+# lst2 = [] 
+# for i in lst1:
+#     if i[0].lower () == 'a' or i[0].lower () == 'f':
+#         lst2.append(i)
+# print(lst2)
+
+
+# 2.m
+# list1 = ["molas", "lug", "apmal", "hsoyuq"]
+# list2 = []
+
+# for i in list1:
+#     list2.append(i[::-1])
+# print(list2)
