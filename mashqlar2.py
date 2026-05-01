cade0b7f770dd8a699a8c14be43d76458575981c
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-05-01 16:51:10 +0500

    uy ishi2

---
+++ mashqlar2.py
@@ -0,0 +1,43 @@
+import os
+os.system("cls")
+ 
+# args  va  kwargs  va lambda
+
+
+# 1.m
+# def  fun1(*a):
+#     a = sum(a)/len(a)
+#     return a
+# s = fun1(2,3,4,7,8,6,5) 
+# print(s)
+
+# 2.m
+# def func(umumiy,chegirma=0):
+#     if umumiy!=0 and chegirma !=0:
+#         print(umumiy - chegirma)
+#     if umumiy!=0 and chegirma ==0:
+#         print(umumiy)
+    
+# func(umumiy = 550,chegirma = 70)
+
+
+# 3.m
+# func = lambda a,b:a if a>b else b 
+
+# print(func(45,56))
+
+    
+# 4.m
+# func = lambda *a: sum(a)/len(a)
+
+# print(func(1,2,3,4,5,6,7,8,9,10))
+
+
+# 5.m
+# def func(set1,set2):
+#     set3 = set1.intersection(set2)
+#     return sum(set3)
+
+# set1 = {1,2,3,4,5,6}
+# set2 = {4,5,6,7,8,9}
+# print(func(set1,set2))
